d000e1d171bdce38b336bf922478c2f8396aa7a9
IftekharulAlam <iftekharulalam38@gmail.com>
2022-11-17 01:37:28 +0600

    joystick added

--- lib/carScreen.dart
+++ lib/carScreen.dart
@@ -1,9 +1,11 @@
 // // ignore_for_file: sort_child_properties_last,, use_build_context_synchronously
 
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_joystick/flutter_joystick.dart';
 import 'dart:io';
 import 'dart:typed_data';
 
@@ -24,13 +26,14 @@ class _carScreenState extends State<carScreen> {
   }
 
   void timerfunction() {
-    mytimer2 = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+    mytimer2 = Timer.periodic(const Duration(milliseconds: 1), (timer) {
       writeData("1", "S");
+      print("Stop");
     });
   }
 
   void writeData(String deviceID, String command) {
-    Uint8List rawAddress = Uint8List.fromList([192, 168, 0, 102]);
+    Uint8List rawAddress = Uint8List.fromList([192, 168, 0, 104]);
     RawDatagramSocket.bind(InternetAddress.anyIPv4, 0)
         .then((RawDatagramSocket socket) {
       // print('Sending from ${socket.address.address}:${socket.port}');
@@ -62,128 +65,192 @@ class _carScreenState extends State<carScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () {
-                        mytimer2.cancel();
-                        writeData("1", "B");
-                        timerfunction();
-                      },
-                      onLongPressStart: (detail) {
-                        mytimer2.cancel();
-                        setState(() {
-                          mytimer = Timer.periodic(const Duration(), (t) {
-                            writeData("1", "B");
-                          });
-                        });
-                      },
-                      onLongPressEnd: (detail) {
-                        mytimer.cancel();
-                        timerfunction();
-                      },
-                      child: Container(
-                        height: 250,
-                        //width: 180,
-                        padding: const EdgeInsets.all(10),
-                        child: const Icon(
-                          Icons.arrow_back,
-                          size: 100,
-                        ),
-                      ),
-                    ),
+                  Joystick(
+                    mode: JoystickMode.horizontalAndVertical,
+                    period: Duration(),
+                    onStickDragEnd: () {
+                      timerfunction();
+                    },
+                    listener: (details) {
+                      setState(() {
+                        if (details.x > 0.3 && details.y == 0.0) {
+                          //Up
+                          print("UP");
+                          mytimer2.cancel();
+                          // setState(() {
+                          //   mytimer = Timer.periodic(const Duration(), (t) {
+                          //     writeData("1", "F");
+                          //   });
+                          // });
+                          writeData("1", "F");
+                        }
+                        if (details.x < -0.3 && details.y == 0.0) {
+                          //Down
+                          print("down");
+                          mytimer2.cancel();
+                          // setState(() {
+                          //   mytimer = Timer.periodic(const Duration(), (t) {
+                          //     writeData("1", "B");
+                          //   });
+                          // });
+                          writeData("1", "B");
+                        }
+                        if (details.x == 0.0 && details.y < 0.3) {
+                          //Left
+                          print("Left");
+                          mytimer2.cancel();
+                          // setState(() {
+                          //   mytimer = Timer.periodic(const Duration(), (t) {
+                          //     writeData("1", "L");
+                          //   });
+                          // });
+                          writeData("1", "L");
+                        }
+                        if (details.x == 0.0 && details.y > 0.3) {
+                          //Right
+                          print("Right");
+                          mytimer2.cancel();
+                          // setState(() {
+                          //   mytimer = Timer.periodic(const Duration(), (t) {
+                          //     writeData("1", "R");
+                          //   });
+                          // });
+                          writeData("1", "R");
+                        }
+                      });
+                    },
                   ),
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () {
-                        mytimer2.cancel();
-                        writeData("1", "F");
-                        timerfunction();
-                      },
-                      onLongPressStart: (detail) {
-                        mytimer2.cancel();
-                        setState(() {
-                          mytimer = Timer.periodic(const Duration(), (t) {
-                            writeData("1", "F");
-                          });
-                        });
-                      },
-                      onLongPressEnd: (detail) {
-                        mytimer.cancel();
-                        timerfunction();
-                      },
-                      child: Container(
-                        height: 250,
-                        //width: 180,
-                        padding: const EdgeInsets.all(10),
-                        child: const Icon(
-                          Icons.arrow_forward,
-                          size: 100,
-                        ),
-                      ),
-                    ),
-                  ),
+                  // Expanded(
+                  //   child: GestureDetector(
+                  //     onTap: () {
+                  //       mytimer2.cancel();
+                  //       writeData("1", "B");
+                  //       timerfunction();
+                  //     },
+                  //     onTapUp: (details) {
+                  //       mytimer.cancel();
+                  //       timerfunction();
+                  //     },
+                  //     onLongPressStart: (detail) {
+                  //       mytimer2.cancel();
+                  //       setState(() {
+                  //         mytimer = Timer.periodic(const Duration(), (t) {
+                  //           writeData("1", "B");
+                  //         });
+                  //       });
+                  //     },
+                  //     onLongPressEnd: (detail) {
+                  //       mytimer.cancel();
+                  //       timerfunction();
+                  //     },
+                  //     child: Container(
+                  //       height: 250,
+                  //       color: Colors.green,
+                  //       //width: 180,
+                  //       padding: const EdgeInsets.all(10),
+                  //       child: const Icon(
+                  //         Icons.arrow_back,
+                  //         size: 100,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
+                  // Expanded(
+                  //   child: GestureDetector(
+                  //     onTap: () {
+                  //       mytimer2.cancel();
+                  //       writeData("1", "F");
+                  //       timerfunction();
+                  //     },
+                  //     onLongPressStart: (detail) {
+                  //       mytimer2.cancel();
+                  //       setState(() {
+                  //         mytimer = Timer.periodic(
+                  //             const Duration(milliseconds: 1), (t) {
+                  //           writeData("1", "F");
+                  //         });
+                  //       });
+                  //     },
+                  //     onLongPressEnd: (detail) {
+                  //       mytimer.cancel();
+                  //       timerfunction();
+                  //     },
+                  //     child: Container(
+                  //       height: 250,
+                  //       //width: 180,
+                  //       color: Colors.green,
+                  //       padding: const EdgeInsets.all(10),
+                  //       child: const Icon(
+                  //         Icons.arrow_forward,
+                  //         size: 100,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
                 ],
               ),
-              Expanded(
-                child: GestureDetector(
-                  onTap: () {
-                    mytimer2.cancel();
-                    writeData("1", "L");
-                    timerfunction();
-                  },
-                  onLongPressStart: (detail) {
-                    mytimer2.cancel();
-                    setState(() {
-                      mytimer = Timer.periodic(const Duration(), (t) {
-                        writeData("1", "L");
-                      });
-                    });
-                  },
-                  onLongPressEnd: (detail) {
-                    mytimer.cancel();
-                    timerfunction();
-                  },
-                  child: Container(
-                    height: 250,
-                    //width: 180,
-                    padding: const EdgeInsets.all(10),
-                    child: const Icon(
-                      Icons.arrow_upward,
-                      size: 100,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: GestureDetector(
-                  onTap: () {
-                    mytimer2.cancel();
-                    writeData("1", "R");
-                    timerfunction();
-                  },
-                  onLongPressStart: (detail) {
-                    mytimer2.cancel();
-                    setState(() {
-                      mytimer = Timer.periodic(const Duration(), (t) {
-                        writeData("1", "R");
-                      });
-                    });
-                  },
-                  onLongPressEnd: (detail) {
-                    mytimer.cancel();
-                    timerfunction();
-                  },
-                  child: Container(
-                    height: 250,
-                    //width: 180,
-                    padding: const EdgeInsets.all(10),
-                    child: const Icon(
-                      Icons.arrow_downward,
-                      size: 100,
-                    ),
-                  ),
-                ),
-              ),
+              // Expanded(
+              //   child: GestureDetector(
+              //     onTap: () {
+              //       mytimer2.cancel();
+              //       writeData("1", "L");
+              //       timerfunction();
+              //     },
+              //     onLongPressStart: (detail) {
+              //       mytimer2.cancel();
+              //       setState(() {
+              //         mytimer = Timer.periodic(const Duration(), (t) {
+              //           writeData("1", "L");
+              //         });
+              //       });
+              //     },
+              //     onLongPressEnd: (detail) {
+              //       mytimer.cancel();
+              //       timerfunction();
+              //     },
+              //     child: Container(
+              //       height: 250,
+              //       //width: 180,
+              //       color: Colors.green,
+              //       padding: const EdgeInsets.all(10),
+              //       child: const Icon(
+              //         Icons.arrow_upward,
+              //         size: 100,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              // Expanded(
+              //   child: GestureDetector(
+              //     onTap: () {
+              //       mytimer2.cancel();
+              //       writeData("1", "R");
+              //       timerfunction();
+              //     },
+              //     onLongPressStart: (detail) {
+              //       mytimer2.cancel();
+              //       setState(() {
+              //         mytimer = Timer.periodic(const Duration(), (t) {
+              //           writeData("1", "R");
+              //         });
+              //       });
+              //     },
+              //     onLongPressEnd: (detail) {
+              //       mytimer.cancel();
+              //       timerfunction();
+              //     },
+              //     child: Container(
+              //       height: 250,
+              //       //width: 180,
+              //       color: Colors.green,
+              //       padding: const EdgeInsets.all(10),
+              //       child: const Icon(
+              //         Icons.arrow_downward,
+              //         size: 100,
+              //       ),
+              //     ),
+              //   ),
+              // ),
             ],
           ),
         ),
