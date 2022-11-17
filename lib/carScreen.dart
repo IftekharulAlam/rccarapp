7612fa729522a564b9338ce1b1fba447ea6a6f46
IftekharulAlam <iftekharulalam38@gmail.com>
2022-11-17 13:38:00 +0600

    joystick with button added

--- lib/carScreen.dart
+++ lib/carScreen.dart
@@ -28,12 +28,12 @@ class _carScreenState extends State<carScreen> {
   void timerfunction() {
     mytimer2 = Timer.periodic(const Duration(milliseconds: 1), (timer) {
       writeData("1", "S");
-      print("Stop");
+      //print("Stop");
     });
   }
 
   void writeData(String deviceID, String command) {
-    Uint8List rawAddress = Uint8List.fromList([192, 168, 0, 104]);
+    Uint8List rawAddress = Uint8List.fromList([192, 168, 0, 102]);
     RawDatagramSocket.bind(InternetAddress.anyIPv4, 0)
         .then((RawDatagramSocket socket) {
       // print('Sending from ${socket.address.address}:${socket.port}');
@@ -58,201 +58,74 @@ class _carScreenState extends State<carScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListView(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Joystick(
-                    mode: JoystickMode.horizontalAndVertical,
-                    period: Duration(),
-                    onStickDragEnd: () {
-                      timerfunction();
-                    },
-                    listener: (details) {
-                      setState(() {
-                        if (details.x > 0.3 && details.y == 0.0) {
-                          //Up
-                          print("UP");
-                          mytimer2.cancel();
-                          // setState(() {
-                          //   mytimer = Timer.periodic(const Duration(), (t) {
-                          //     writeData("1", "F");
-                          //   });
-                          // });
-                          writeData("1", "F");
-                        }
-                        if (details.x < -0.3 && details.y == 0.0) {
-                          //Down
-                          print("down");
-                          mytimer2.cancel();
-                          // setState(() {
-                          //   mytimer = Timer.periodic(const Duration(), (t) {
-                          //     writeData("1", "B");
-                          //   });
-                          // });
-                          writeData("1", "B");
-                        }
-                        if (details.x == 0.0 && details.y < 0.3) {
-                          //Left
-                          print("Left");
-                          mytimer2.cancel();
-                          // setState(() {
-                          //   mytimer = Timer.periodic(const Duration(), (t) {
-                          //     writeData("1", "L");
-                          //   });
-                          // });
-                          writeData("1", "L");
-                        }
-                        if (details.x == 0.0 && details.y > 0.3) {
-                          //Right
-                          print("Right");
-                          mytimer2.cancel();
-                          // setState(() {
-                          //   mytimer = Timer.periodic(const Duration(), (t) {
-                          //     writeData("1", "R");
-                          //   });
-                          // });
-                          writeData("1", "R");
-                        }
-                      });
-                    },
-                  ),
-                  // Expanded(
-                  //   child: GestureDetector(
-                  //     onTap: () {
-                  //       mytimer2.cancel();
-                  //       writeData("1", "B");
-                  //       timerfunction();
-                  //     },
-                  //     onTapUp: (details) {
-                  //       mytimer.cancel();
-                  //       timerfunction();
-                  //     },
-                  //     onLongPressStart: (detail) {
-                  //       mytimer2.cancel();
-                  //       setState(() {
-                  //         mytimer = Timer.periodic(const Duration(), (t) {
-                  //           writeData("1", "B");
-                  //         });
-                  //       });
-                  //     },
-                  //     onLongPressEnd: (detail) {
-                  //       mytimer.cancel();
-                  //       timerfunction();
-                  //     },
-                  //     child: Container(
-                  //       height: 250,
-                  //       color: Colors.green,
-                  //       //width: 180,
-                  //       padding: const EdgeInsets.all(10),
-                  //       child: const Icon(
-                  //         Icons.arrow_back,
-                  //         size: 100,
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                  // Expanded(
-                  //   child: GestureDetector(
-                  //     onTap: () {
-                  //       mytimer2.cancel();
-                  //       writeData("1", "F");
-                  //       timerfunction();
-                  //     },
-                  //     onLongPressStart: (detail) {
-                  //       mytimer2.cancel();
-                  //       setState(() {
-                  //         mytimer = Timer.periodic(
-                  //             const Duration(milliseconds: 1), (t) {
-                  //           writeData("1", "F");
-                  //         });
-                  //       });
-                  //     },
-                  //     onLongPressEnd: (detail) {
-                  //       mytimer.cancel();
-                  //       timerfunction();
-                  //     },
-                  //     child: Container(
-                  //       height: 250,
-                  //       //width: 180,
-                  //       color: Colors.green,
-                  //       padding: const EdgeInsets.all(10),
-                  //       child: const Icon(
-                  //         Icons.arrow_forward,
-                  //         size: 100,
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                child: const Text('Connect'),
+                onPressed: () {
+                  // writeData(dropdownvalue, myip.text);
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => const carScreen()),
+                  // );
+                },
               ),
-              // Expanded(
-              //   child: GestureDetector(
-              //     onTap: () {
-              //       mytimer2.cancel();
-              //       writeData("1", "L");
-              //       timerfunction();
-              //     },
-              //     onLongPressStart: (detail) {
-              //       mytimer2.cancel();
-              //       setState(() {
-              //         mytimer = Timer.periodic(const Duration(), (t) {
-              //           writeData("1", "L");
-              //         });
-              //       });
-              //     },
-              //     onLongPressEnd: (detail) {
-              //       mytimer.cancel();
-              //       timerfunction();
-              //     },
-              //     child: Container(
-              //       height: 250,
-              //       //width: 180,
-              //       color: Colors.green,
-              //       padding: const EdgeInsets.all(10),
-              //       child: const Icon(
-              //         Icons.arrow_upward,
-              //         size: 100,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-              // Expanded(
-              //   child: GestureDetector(
-              //     onTap: () {
-              //       mytimer2.cancel();
-              //       writeData("1", "R");
-              //       timerfunction();
-              //     },
-              //     onLongPressStart: (detail) {
-              //       mytimer2.cancel();
-              //       setState(() {
-              //         mytimer = Timer.periodic(const Duration(), (t) {
-              //           writeData("1", "R");
-              //         });
-              //       });
-              //     },
-              //     onLongPressEnd: (detail) {
-              //       mytimer.cancel();
-              //       timerfunction();
-              //     },
-              //     child: Container(
-              //       height: 250,
-              //       //width: 180,
-              //       color: Colors.green,
-              //       padding: const EdgeInsets.all(10),
-              //       child: const Icon(
-              //         Icons.arrow_downward,
-              //         size: 100,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Joystick(
+                  // stick: Container(
+                  //   width: 50,
+                  //   height: 50,
+                  //   color:Color.fromARGB(31, 19, 1, 1),
+                  // ),
+                  mode: JoystickMode.horizontalAndVertical,
+                  // period: Duration(),
+                  onStickDragEnd: () {
+                    timerfunction();
+                  },
+                  listener: (details) {
+                    setState(() {
+                      if (details.x > 0.3 && details.y == 0.0) {
+                        //Up
+                        //print("UP");
+                        mytimer2.cancel();
+
+                        writeData("1", "F");
+                      }
+                      if (details.x < -0.3 && details.y == 0.0) {
+                        //Down
+                        //print("down");
+                        mytimer2.cancel();
+
+                        writeData("1", "B");
+                      }
+                      if (details.x == 0.0 && details.y < 0.3) {
+                        //Left
+                        //print("Left");
+                        mytimer2.cancel();
+
+                        writeData("1", "L");
+                      }
+                      if (details.x == 0.0 && details.y > 0.3) {
+                        //Right
+                        //print("Right");
+                        mytimer2.cancel();
+
+                        writeData("1", "R");
+                      }
+                    });
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
